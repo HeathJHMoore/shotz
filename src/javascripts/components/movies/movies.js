b04075c6be6d782582894bf824e06c257ac51868
Heath JH Moore <heathjhmoore@gmail.com>
2019-04-22 18:54:33 -0500

    made movie cards

--- src/javascripts/components/movies/movies.js
+++ src/javascripts/components/movies/movies.js
@@ -8,7 +8,15 @@ let movies = [];
 const domStringBuilder = () => {
   let domString = '';
   movies.forEach((movie) => {
-    domString += `<h3>${movie.name}</h3>`;
+    domString += `<div class="card col-12 col-sm-6 col-md-6 col-lg-4" id="${movie.id}">`;
+    domString += '<div class="card-body text-center">';
+    domString += `<h2 class="card-title" style="border-bottom: solid 3px black;">${movie.name}</h2>`;
+    domString += `<h4>Genre: ${movie.genre}</h4>`;
+    domString += `<p class="card-text">Summary: ${movie.description}</p>`;
+    domString += `<p class= "card-text" style="font-weight: bold; border: solid 1px black; background-color: black; color:white;">Release Date: ${movie.releaseDate}</p>`;
+    domString += `<p class= "card-text"> Locations: ${movie.locations.length}</p>`;
+    domString += '</div>';
+    domString += '</div>';
   });
   util.printToDom('movies', domString);
 };
