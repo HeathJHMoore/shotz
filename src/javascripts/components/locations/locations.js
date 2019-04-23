4b380afbeb16dc84e8c9259916512609a4ec89b8
Heath JH Moore <heathjhmoore@gmail.com>
2019-04-22 19:26:27 -0500

    added card styling to change color on shoot time

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -5,10 +5,30 @@ import './locations.scss';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-warning';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+  }
+  return selectedClass;
+};
+
 const domStringBuilder = () => {
   let domString = '';
   locations.forEach((location) => {
-    domString += `<div class="card col-12 col-sm-8 col-md-6 col-lg-4 col-xl-2" id="${location.id}" style="width: 18rem;">`;
+    domString += `<div class="card col-12 col-sm-8 col-md-6 col-lg-4 col-xl-2 ${shootTimeClass(location.shootTime)}" id="${location.id}" style="width: 18rem;">`;
     domString += `<img class="card-img-top" src="${location.imageUrl}" alt="Card image cap">`;
     domString += '<div class="card-body">';
     domString += `<h4 class="card-title">${location.name}</h4>`;
